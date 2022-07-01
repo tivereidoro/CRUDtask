bc7d0315ffa046542e55249e0f9cf8371768eed9
Tivere IDORO <105525310+tivereidoro@users.noreply.github.com>
2022-07-01 21:20:20 +0100

    update views

--- I4G02299876A/blog/models.py
+++ I4G02299876A/blog/models.py
@@ -5,8 +5,9 @@ from django.contrib.auth import get_user_model
 from django.utils import timezone
 from django.urls import reverse
 from django.template.defaultfilters import slugify
-# Create your models here.
 
+
+# Create your models here.
 
 class Post(models.Model):
 
--- I4G02299876A/blog/views.py
+++ I4G02299876A/blog/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
 
 # Create your views here.
+from ast import Delete
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+
+from .models import Post
+
+
+class PostListView(ListView):
+    model = Post
+
+    template_name = 'blog/post_list.html'
+
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+    template_name = 'blog/post_form.html'
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+    template_name = 'blog/post_form.html'
+
+
+class PostDeleteView(DeleteView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+    template_name = 'blog/post_confirm_delete.html'
